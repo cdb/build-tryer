4f2cf6107a3ae383d8a4a2af23ab088aae1cb28c
Cameron Booth <cdb@github.com>
2019-09-04 14:44:29 -0700

    Edit to trigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
+        <p>Hello World including Azure Pipelines</p>
         <a
           className="App-link"
           href="https://reactjs.org"
